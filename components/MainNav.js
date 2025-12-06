607c37b861f0551ef71a5d95d61849cccc68b6f5
Tehlaran <teslara18@gmail.com>
2025-12-05 21:09:18 -0500

    Update MainNav navigation links and login/register UI

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,21 +1,119 @@
-
+// components/MainNav.js
 import Link from "next/link";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown
+} from "react-bootstrap";
+
+import {
+  isAuthenticated,
+  readToken,
+  removeToken
+} from "@/lib/authenticate";
+
+import { useAtom } from "jotai";
+import { favouritesAtom, searchHistoryAtom } from "@/store";
 
 export default function MainNav() {
+  const router = useRouter();
+
+  const [, setFavouritesList] = useAtom(favouritesAtom);
+  const [, setSearchHistory] = useAtom(searchHistoryAtom);
+
+  const [user, setUser] = useState(null);
+
+  // Update navbar when route changes or auth changes
+  useEffect(() => {
+    if (isAuthenticated()) {
+      const token = readToken();
+      setUser(token?.userName || null);
+    } else {
+      setUser(null);
+    }
+  }, [router.pathname]);
+
+  const logout = () => {
+    removeToken();
+    setFavouritesList([]);
+    setSearchHistory([]);
+    setUser(null);
+    router.push("/login");
+  };
+
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark" className="fixed-top">
-        <Container>
-          <Navbar.Brand as={Link} href="/">Esther Nascimento</Navbar.Brand>
+    <Navbar bg="light" expand="lg" className="mb-4">
+      <Container>
+        <Link href="/" passHref legacyBehavior>
+          <Navbar.Brand>My Books App</Navbar.Brand>
+        </Link>
+
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
           <Nav className="me-auto">
-            <Nav.Link as={Link} href="/about">About</Nav.Link>
-            <Nav.Link as={Link} href="/favourites">Favourites</Nav.Link>
+
+            {/* Home */}
+            <Link href="/" passHref legacyBehavior>
+              <Nav.Link active={router.pathname === "/"}>Home</Nav.Link>
+            </Link>
+
+            {/* Books Page (keep if you had it) */}
+            <Link href="/books" passHref legacyBehavior>
+              <Nav.Link active={router.pathname === "/books"}>Books</Nav.Link>
+            </Link>
+
+            {/* About Page (if you had it) */}
+            <Link href="/about" passHref legacyBehavior>
+              <Nav.Link active={router.pathname === "/about"}>About</Nav.Link>
+            </Link>
+
+            {/* Favourites */}
+            <Link href="/favourites" passHref legacyBehavior>
+              <Nav.Link active={router.pathname === "/favourites"}>
+                Favourites
+              </Nav.Link>
+            </Link>
           </Nav>
-        </Container>
-      </Navbar>
-      <br />
-      <br />
-    </>
+
+          <Nav>
+            {/* LOGGED OUT -> Show Login + Register */}
+            {!user && (
+              <>
+                <Link href="/login" passHref legacyBehavior>
+                  <Nav.Link active={router.pathname === "/login"}>
+                    Login
+                  </Nav.Link>
+                </Link>
+
+                <Link href="/register" passHref legacyBehavior>
+                  <Nav.Link active={router.pathname === "/register"}>
+                    Register
+                  </Nav.Link>
+                </Link>
+              </>
+            )}
+
+            {/* LOGGED IN -> Show Username Dropdown */}
+            {user && (
+              <NavDropdown title={user} align="end">
+
+                <Link href="/favourites" passHref legacyBehavior>
+                  <NavDropdown.Item>Favourites</NavDropdown.Item>
+                </Link>
+
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item onClick={logout}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
